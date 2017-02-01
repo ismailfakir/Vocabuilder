84bf164809116c82e2c1f80a4ab7f3541ff9008e
ismail Fakir <md.ismail.fakir@gmail.com>
2017-02-01 14:31:38 +0100

    Quiz result activity updated

--- app/src/main/java/net/cloudcentrik/wordplus/QuizResultActivity.java
+++ app/src/main/java/net/cloudcentrik/wordplus/QuizResultActivity.java
@@ -22,7 +22,9 @@ import org.w3c.dom.Text;
 import java.util.ArrayList;
 
 public class QuizResultActivity extends AppCompatActivity {
-    TextView txtResult1,txtResult2,txtResult3;
+    //TextView txtResult1,txtResult2,txtResult3;
+    ImageView imgResult1,imgResult2,imgResult3;
+    TextView txtResultUserAnswer1,txtResultUserAnswer2,txtResultUserAnswer3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +51,7 @@ public class QuizResultActivity extends AppCompatActivity {
         //Answer a=(Answer) answersList.get(1);
         //Log.d("Answer",""+a.toString());
 
-        txtScore.setText("Your Score :"+countScore(answersList));
+
 
         TextView txtResultQuestion1=(TextView)findViewById(R.id.txtResultQuestion1);
         TextView txtResultQuestion2=(TextView)findViewById(R.id.txtResultQuestion2);
@@ -68,22 +70,28 @@ public class QuizResultActivity extends AppCompatActivity {
         txtResultCorrectAnswer2.setText(answersList.get(1).getCorrectAnswer());
         txtResultCorrectAnswer3.setText(answersList.get(2).getCorrectAnswer());
 
-        TextView txtResultUserAnswer1=(TextView)findViewById(R.id.txtResultUserAnswer1);
-        TextView txtResultUserAnswer2=(TextView)findViewById(R.id.txtResultUserAnswer2);
-        TextView txtResultUserAnswer3=(TextView)findViewById(R.id.txtResultUserAnswer3);
+         txtResultUserAnswer1=(TextView)findViewById(R.id.txtResultUserAnswer1);
+         txtResultUserAnswer2=(TextView)findViewById(R.id.txtResultUserAnswer2);
+         txtResultUserAnswer3=(TextView)findViewById(R.id.txtResultUserAnswer3);
 
         txtResultUserAnswer1.setText(answersList.get(0).getUserAnswer());
         txtResultUserAnswer2.setText(answersList.get(1).getUserAnswer());
         txtResultUserAnswer3.setText(answersList.get(2).getUserAnswer());
 
 
-        txtResult1=(TextView)findViewById(R.id.txtResult1);
+        /*txtResult1=(TextView)findViewById(R.id.txtResult1);
         txtResult2=(TextView)findViewById(R.id.txtResult2);
         txtResult3=(TextView)findViewById(R.id.txtResult3);
 
         txtResult1.setText(answersList.get(0).getResult());
         txtResult2.setText(answersList.get(1).getResult());
-        txtResult3.setText(answersList.get(2).getResult());
+        txtResult3.setText(answersList.get(2).getResult());*/
+
+        imgResult1=(ImageView)findViewById(R.id.imgResult1);
+        imgResult2=(ImageView)findViewById(R.id.imgResult2);
+        imgResult3=(ImageView)findViewById(R.id.imgResult3);
+
+        txtScore.setText("Your Score :"+countScore(answersList));
 
     }
 
@@ -92,12 +100,29 @@ public class QuizResultActivity extends AppCompatActivity {
         Log.d("Size",""+answerArrayList.size());
         int score=0;
 
+        Drawable drawableRight = ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_right_green_24dp);
+        Drawable drawableWrong = ContextCompat.getDrawable(getApplicationContext(),R.drawable.ic_wrong_red_24dp);
         for(int i=0;i<answerArrayList.size();i++){
 
             Answer a=(Answer) answerArrayList.get(i);
 
             if(a.getResult().equals("correct")){
                 score++;
+                switch (i){
+                    case 0:
+                        txtResultUserAnswer1.setTextColor(Color.parseColor("#2e7d32"));
+                        imgResult1.setImageDrawable(drawableRight);
+
+                        break;
+                    case 1:
+                        txtResultUserAnswer2.setTextColor(Color.parseColor("#2e7d32"));
+                        imgResult2.setImageDrawable(drawableRight);
+                        break;
+                    case 2:
+                        txtResultUserAnswer3.setTextColor(Color.parseColor("#2e7d32"));
+                        imgResult3.setImageDrawable(drawableRight);
+                        break;
+                }
             }
         }
 
